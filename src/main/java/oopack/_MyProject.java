e6c5d435dbc2cce589b93fa9cff54cd64da2c111
asdru22 <76167257+asdru22@users.noreply.github.com>
2025-04-10 19:20:50 +0200

    fixed gitignore

---
+++ src/main/java/oopack/_MyProject.java
@@ -0,0 +1,57 @@
+package oopack;
+
+import oopack.datapack.Data;
+import oopack.datapack.DataEntries;
+import oopack.datapack.Datapack;
+import oopack.resourcepack.AssetEntries;
+import oopack.resourcepack.Assets;
+import oopack.resourcepack.Resourcepack;
+import oopack.resourcepack.objects.Lang;
+import oopack.resourcepack.objects.SoundItem;
+import oopack.resourcepack.objects.Sounds;
+
+public class _MyProject {
+    public static void main(String[] args) {
+
+        String author = "Asdru";
+
+        Namespace main = new Namespace("proj");
+        Namespace library = new Namespace("other");
+
+        Datapack datapack = new Datapack(main, library);
+        Resourcepack resourcepack = new Resourcepack(main);
+
+        String mcmeta = String.format("""
+                {
+                    "pack": {
+                        "pack_format":  52,
+                        "description": "by %s"
+                    }
+                }
+                """, author);
+
+        datapack.setPackMCMeta(mcmeta);
+        resourcepack.setPackMCMeta(mcmeta);
+
+        Data mainData = main.getData();
+        mainData.add(DataEntries.FUNCTION, "among", "say hi", "say lol");
+
+        Assets mainAssets = main.getAssets();
+        mainAssets.add(AssetEntries.TEXTURES, "hello", "img");
+
+        Lang lang = mainAssets.getLang();
+        lang.add("item.test", "Test");
+
+        Sounds sounds = mainAssets.getSounds();
+        sounds.add("item.double_edged_sword.throw", "basadlsadlsa",
+                new SoundItem("test/balls", "sound"));
+
+        Project p = new Project("My new project", "latest",
+                "C:\\Users\\Ale\\Documents\\java projects\\OOPack\\out\\built", "test");
+
+        p.setDatapack(datapack);
+        p.setResourcepack(resourcepack);
+
+        p.build();
+    }
+}
