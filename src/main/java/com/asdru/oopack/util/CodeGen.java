c6ef59aaa422ff3a704141854dc7af99739ebf77
asdru22 <76167257+asdru22@users.noreply.github.com>
2025-09-11 22:49:27 +0200

    safety commit before codegen

--- src/main/java/com/asdru/oopack/util/CodeGen.java
+++ src/main/java/com/asdru/oopack/util/CodeGen.java
@@ -1,112 +1,118 @@
 package com.asdru.oopack.util;
 
-import com.asdru.oopack.internal.VersionInfo;
 import com.squareup.javapoet.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
+
 import javax.lang.model.element.Modifier;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class CodeGen {
 
-    private static final String
-            packageName = "com.asdru.oopack",
-            fileName = "Version";
+    private static final String packageName = "com.asdru.oopack.objects";
+    private static final Path outputDir = Path.of("./src/main/java");
 
-    public static void generateVersionsEnum(Map<String, VersionInfo> versionInfoMap) throws Exception {
-        // Create enum builder
-        TypeSpec.Builder enumBuilder = TypeSpec.enumBuilder(fileName)
-                .addModifiers(Modifier.PUBLIC)
-                .addField(String.class, "id", Modifier.PRIVATE, Modifier.FINAL)
-                .addField(int.class, "datapackVersion", Modifier.PRIVATE, Modifier.FINAL)
-                .addField(int.class, "datapackVersionMinor", Modifier.PRIVATE, Modifier.FINAL)
-                .addField(int.class, "resourcepackVersion", Modifier.PRIVATE, Modifier.FINAL)
-                .addField(int.class, "resourcepackVersionMinor", Modifier.PRIVATE, Modifier.FINAL)
-                .addField(int.class, "dataVersion", Modifier.PRIVATE, Modifier.FINAL)
-                .addMethod(MethodSpec.constructorBuilder()
-                        .addParameter(String.class, "id")
-                        .addParameter(int.class, "datapackVersion")
-                        .addParameter(int.class, "datapackVersionMinor")
-                        .addParameter(int.class, "resourcepackVersion")
-                        .addParameter(int.class, "resourcepackVersionMinor")
-                        .addParameter(int.class, "dataVersion")
-                        .addStatement("this.id = id")
-                        .addStatement("this.datapackVersion = datapackVersion")
-                        .addStatement("this.datapackVersionMinor = datapackVersionMinor")
-                        .addStatement("this.resourcepackVersion = resourcepackVersion")
-                        .addStatement("this.resourcepackVersionMinor = resourcepackVersionMinor")
-                        .addStatement("this.dataVersion = dataVersion")
-                        .build()
-                );
+    public static void gen(
+            String className,
+            String superClassName,
+            String folderName
+    ) throws IOException {
 
-        // Add getters
-        enumBuilder.addMethod(MethodSpec.methodBuilder("getId")
+        // imports
+        ClassName jsonObject = ClassName.get("com.google.gson", "JsonObject");
+        ClassName jsonFileFactory = ClassName.get("com.asdru.oopack.internal", "JsonFileFactory");
+        ClassName superClass = ClassName.get("com.asdru.oopack.objects", superClassName);
+
+        // static factory
+        TypeName factoryType = ParameterizedTypeName.get(jsonFileFactory, ClassName.get(packageName, className));
+
+        FieldSpec factoryField = FieldSpec.builder(factoryType, "f", Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
+                .initializer("new JsonFile.Factory<>($T.class)", ClassName.get(packageName, className))
+                .build();
+
+        // Protected constructor
+        MethodSpec constructor = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PROTECTED)
+                .addParameter(String.class, "name")
+                .addParameter(jsonObject, "content")
+                .addStatement("super(name, content)")
+                .build();
+
+        MethodSpec getFolderName = MethodSpec.methodBuilder("getFolderName")
+                .addAnnotation(Override.class)
                 .addModifiers(Modifier.PUBLIC)
                 .returns(String.class)
-                .addStatement("return this.id")
-                .build());
-
-        enumBuilder.addMethod(MethodSpec.methodBuilder("getDatapackVersion")
-                .addModifiers(Modifier.PUBLIC)
-                .returns(int.class)
-                .addStatement("return this.datapackVersion")
-                .build());
-
-        enumBuilder.addMethod(MethodSpec.methodBuilder("getDatapackVersionMinor")
-                .addModifiers(Modifier.PUBLIC)
-                .returns(int.class)
-                .addStatement("return this.datapackVersionMinor")
-                .build());
-
-        enumBuilder.addMethod(MethodSpec.methodBuilder("getResourcepackVersion")
-                .addModifiers(Modifier.PUBLIC)
-                .returns(int.class)
-                .addStatement("return this.resourcepackVersion")
-                .build());
-
-        enumBuilder.addMethod(MethodSpec.methodBuilder("getResourcepackVersionMinor")
-                .addModifiers(Modifier.PUBLIC)
-                .returns(int.class)
-                .addStatement("return this.resourcepackVersionMinor")
-                .build());
-
-        // Get latest version
-        Map.Entry<String, VersionInfo> firstEntry = versionInfoMap.entrySet().iterator().next();
-        // Add the "LATEST" constant first
-        enumBuilder.addEnumConstant("LATEST",
-                TypeSpec.anonymousClassBuilder("$S, $L, $L, $L, $L, $L",
-                        firstEntry.getKey(),
-                        firstEntry.getValue().datapackVersion(),
-                        firstEntry.getValue().datapackVersionMinor(),
-                        firstEntry.getValue().resourcepackVersion(),
-                        firstEntry.getValue().resourcepackVersionMinor(),
-                        firstEntry.getValue().dataVersion()
-                ).build()
-        );
-
-        // iterate over the rest of the map and add constants
-        versionInfoMap.forEach((id, info) -> {
-            String enumName = "V_" + id.replace("-", "_").replace(".", "_");
-            enumBuilder.addEnumConstant(enumName,
-                    TypeSpec.anonymousClassBuilder("$S, $L, $L, $L, $L, $L",
-                            id,
-                            info.datapackVersion(),
-                            info.datapackVersionMinor(),
-                            info.resourcepackVersion(),
-                            info.resourcepackVersionMinor(),
-                            info.dataVersion()
-                    ).build()
-            );
-        });
-
-        // Build the enum
-        TypeSpec enumType = enumBuilder.build();
-
-        // Write to target/generated-sources/com/asdru/oopack/Version.java
-        JavaFile javaFile = JavaFile.builder(packageName, enumType)
+                .addStatement("return $S", folderName)
                 .build();
-        Path path = Paths.get("target/generated-sources");
-        javaFile.writeTo(path);
+
+        TypeSpec generatedClass = TypeSpec.classBuilder(className)
+                .addModifiers(Modifier.PUBLIC)
+                .superclass(superClass)
+                .addField(factoryField)
+                .addMethod(constructor)
+                .addMethod(getFolderName)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(packageName, generatedClass)
+                .skipJavaLangImports(true)
+                .build();
+
+        // patch "public class" to "public non-sealed class"
+        String source = javaFile.toString().replace("public class", "public non-sealed class");
+
+        Path outputPath = outputDir.resolve(packageName.replace('.', '/'))
+                .resolve(className + ".java");
+        java.nio.file.Files.createDirectories(outputPath.getParent());
+        java.nio.file.Files.writeString(outputPath, source);
     }
 
+    private static void genData(String className, String folderName) throws IOException {
+        gen(className, "DataJson", folderName);
+    }
+
+    private static void genTag(String className, String foldernName) throws IOException {
+        genData(className + "Tag", "tags/" + foldernName);
+    }
+
+    private static void genAssets(String className, String folderName) throws IOException {
+        gen(className, "AssetsJson", folderName);
+    }
+
+    public static void main(String[] args) throws IOException {
+        genData("Advancement", "advancement");
+        genData("BannerPattern", "banner_pattern");
+        genData("DamageType", "damage_type");
+        genData("Dialog", "dialog");
+        genData("Dimension", "dimension");
+        genData("DimensionType", "dimension_type");
+        genData("Enchantment", "enchantment");
+        genData("EnchantmentProvider", "enchantment_provider");
+        genData("Instrument", "instrument");
+        genData("JukeboxSong", "jukebox_song");
+        genData("LootTable", "loot_table");
+        genData("Recipes", "recipe");
+        genData("TrimMaterial", "trim_material");
+        genData("TrimPattern", "trim_pattern");
+        genData("Worldgen", "worldgen");
+
+        genTag("BannerPattern", "banner_pattern");
+        genTag("Block", "block");
+        genTag("DamageType", "damage_type");
+        genTag("Dialog", "dialog");
+        genTag("Enchantment", "enchantment");
+        genTag("EntityType", "entity_type");
+        genTag("Instrument", "instrument");
+        genTag("Item", "item");
+        genTag("PaintingVariant", "painting_variant");
+        genTag("Worldgen", "worldgen");
+
+        genAssets("Atlas", "atlases");
+        genAssets("Blockstate", "blockstates");
+        genAssets("Equipment", "equipment");
+        genAssets("Font", "font");
+        genAssets("Item", "items");
+        genAssets("Lang", "lang");
+        genAssets("Model", "models");
+
+
+    }
 }
